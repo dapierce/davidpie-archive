c9af4e4900e4ce14f5e194185a16a29edac31a5a
dapierce <davidaaronpierce@gmail.com>
2019-11-27 15:22:55 -0800

    typeface speed improvements, accessibility fix

--- src/components/links.js
+++ src/components/links.js
@@ -13,31 +13,34 @@ const Links = () => (
   <div className="links">
     <ul>
       <li>
-        <a href="https://gitlab.com/dapierce" alt="GitLab">
+        <a href="https://gitlab.com/dapierce" aria-label="GitLab">
           <FontAwesomeIcon icon={faGitlab} />
           <span className="link-label">GitLab</span>
         </a>
       </li>
       <li>
-        <a href="https://github.com/dapierce" alt="GitHub">
+        <a href="https://github.com/dapierce" aria-label="GitHub">
           <FontAwesomeIcon icon={faGithub} />
           <span className="link-label">GitHub</span>
         </a>
       </li>
       <li>
-        <a href="https://www.linkedin.com/in/piercedavid/" alt="LinkedIn">
+        <a
+          href="https://www.linkedin.com/in/piercedavid/"
+          aria-label="LinkedIn"
+        >
           <FontAwesomeIcon icon={faLinkedin} />
           <span className="link-label">LinkedIn</span>
         </a>
       </li>
       <li>
-        <a href="https://twitter.com/da_pierce" alt="Twitter">
+        <a href="https://twitter.com/da_pierce" aria-label="Twitter">
           <FontAwesomeIcon icon={faTwitter} />
           <span className="link-label">Twitter</span>
         </a>
       </li>
       <li>
-        <a href="https://keybase.io/dapierce" alt="Keybase">
+        <a href="https://keybase.io/dapierce" aria-label="Keybase">
           <FontAwesomeIcon icon={faKeybase} />
           <span className="link-label">Keybase</span>
         </a>

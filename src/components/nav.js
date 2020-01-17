965f663b015fcd9c9e53c24e838b188c5b75b468
dapierce <davidaaronpierce@gmail.com>
2020-01-17 15:57:51 -0800

    Nav link active style and npm update.

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,13 +4,24 @@ import React from "react"
 
 import styles from "./styles/nav.module.css"
 
+const activeNavlinkStyle = {
+  color: `var(--text-color)`,
+  padding: `3px 3px`,
+  margin: `0 -3px`,
+  background: `white`,
+  backgroundPosition: `0 calc(100% + 1px)`,
+  borderRadius: `6px`,
+}
+
 const Nav = ({ location }) => (
-  // change Nav if on home page
   <nav className={styles.nav}>
     <div className={styles.navbar}>
       <ul className={styles.title}>
         <li>
-          <Link to="/">
+          <Link
+            to="/"
+            activeStyle={{ backgroundColor: `white`, color: `#444444` }}
+          >
             david
             <span role="img" aria-label="pie">
               🥧
@@ -20,16 +31,24 @@ const Nav = ({ location }) => (
       </ul>
       <ul className={styles.navlinks}>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" activeStyle={activeNavlinkStyle}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/projects">Projects</Link>
+          <Link to="/projects" activeStyle={activeNavlinkStyle}>
+            Projects
+          </Link>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <Link to="/blog" activeStyle={activeNavlinkStyle}>
+            Blog
+          </Link>
         </li>
         <li>
-          <Link to="/resume">Resume</Link>
+          <Link to="/resume" activeStyle={activeNavlinkStyle}>
+            Resume
+          </Link>
         </li>
       </ul>
     </div>
